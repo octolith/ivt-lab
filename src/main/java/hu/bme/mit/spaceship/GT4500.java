ef5d88562ab863cc5f58173bd79b974091bd4843
octolith <mr.marcell.levai@gmail.com>
2020-04-25 19:31:24 +0200

    Fix firingSuccess logic in GT4500 fireTorpedo(ALL)

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,27 +77,30 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
+        bool firingSuccessOfPrimary = false;
+        bool firingSuccessOfSecondary = false;
         // try to fire both of the torpedo stores
         if (wasPrimaryFiredLast) {
           if (! secondaryTorpedoStore.isEmpty()) {
-            firingSuccess = secondaryTorpedoStore.fire(1);
+            firingSuccessOfSecondary = secondaryTorpedoStore.fire(1);
             wasPrimaryFiredLast = false;
           }
           if (! primaryTorpedoStore.isEmpty()) {
-            firingSuccess = primaryTorpedoStore.fire(1);
+            firingSuccessOfPrimary = primaryTorpedoStore.fire(1);
             wasPrimaryFiredLast = true;
           }
         }
         else {
           if (! primaryTorpedoStore.isEmpty()) {
-            firingSuccess = primaryTorpedoStore.fire(1);
+            firingSuccessOfPrimary = primaryTorpedoStore.fire(1);
             wasPrimaryFiredLast = true;
           }
           if (! secondaryTorpedoStore.isEmpty()) {
-            firingSuccess = secondaryTorpedoStore.fire(1);
+            firingSuccessOfSecondary = secondaryTorpedoStore.fire(1);
             wasPrimaryFiredLast = false;
           }
         }
+        firingSuccess = firingSuccessOfPrimary || firingSuccessOfSecondary;
         break;
     }
 
